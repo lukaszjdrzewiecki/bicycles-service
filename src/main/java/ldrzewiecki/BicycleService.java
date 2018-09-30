86ca28f4860a313de04b735a05c92126970f2cd2
vsxxldr <lukasz.drzewiecki@vps.no>
2018-09-30 18:25:16 +0200

    Added bicycleService

---
+++ src/main/java/ldrzewiecki/BicycleService.java
@@ -0,0 +1,16 @@
+package ldrzewiecki;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BicycleService {
+    List<Bicycle> bicycles = new ArrayList();
+
+    public void addBicycle(String name) {
+        Bicycle bicycle = new Bicycle();
+        bicycle.setName(name);
+
+        bicycles.add(bicycle);
+
+    }
+}
