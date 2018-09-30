169e712e59410e40adee314c97245a1347d6b992
vsxxldr <lukasz.drzewiecki@vps.no>
2018-09-30 18:19:29 +0200

    Added bicycle

---
+++ src/main/java/ldrzewiecki/Bicycle.java
@@ -0,0 +1,25 @@
+package ldrzewiecki;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bicycle {
+    private String name;
+    private List<BicyclePart> parts = new ArrayList();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<BicyclePart> getParts() {
+        return parts;
+    }
+
+    public void setParts(List<BicyclePart> parts) {
+        this.parts = parts;
+    }
+}
---
+++ src/main/java/ldrzewiecki/BicyclePart.java
@@ -0,0 +1,23 @@
+package ldrzewiecki;
+
+public class BicyclePart {
+
+    private String category;
+    private String name;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
