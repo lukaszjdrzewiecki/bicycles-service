d258537c38f1037ca71b36b63ec0adcea4ed053e
vsxxldr <lukasz.drzewiecki@vps.no>
2018-10-07 16:45:38 +0200

    Uwagi do kodu - zadania domowe - REFAKTORYZACJA (extract method) + toString

--- src/main/java/ldrzewiecki/Bicycle.java
+++ src/main/java/ldrzewiecki/Bicycle.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bicycle {
+
+    /* TODO - zmienić na prywatne i zastąpić setName oraz getParts().add
+     HERMETYZACJA jedna z podstaw porgamowania obiektowego. Pola, muszą być prywatne, a dostęp do nich przez getEry i setery
+     */
     public String name;
     public List<BicyclePart> parts = new ArrayList();
 
--- src/main/java/ldrzewiecki/BicycleService.java
+++ src/main/java/ldrzewiecki/BicycleService.java
@@ -11,8 +11,16 @@ public class BicycleService {
         Bicycle newBicycle = new Bicycle();
         newBicycle.setName(name);
 
+        /*
+        TODO - masz w dwóch miejscach wyszukanie roweru - w metodzie addBicycle oraz addBicyclePart
+        To oznacza, ze jakbys zmienil bicycles z List, na np.: bazę danych, musiałbyś zrobić zmianę w dwóch miejsca
+        Proces poprawiania jakości kodu nazywa się REFAKTORYZACJA - bardzo ważny termin - spróbuj zastąpić
+        for (Bicycle bicycle : bicycles) metodoą findBicycleByName(), która zwraca rower, lub NULL w przypadku kiedy nie ma roweru o podanej nazwie.
+         */
        for (Bicycle bicycle : bicycles) {
             if(bicycle.getName().equals(name)) {
+                //TODO - tak naprawde do takich rzeczy uzywa sie wyjatkow. Spróbuj zamienić ten pusty return na rzucienie wyjątku RuntimeException
+                // czyli zamiast dwóch ponizszych linii zrób throw new RuntimeException("Nie mozna ...") - daj znac czy rozumiesz
                 System.out.println("Nie mozna dodac dwoch rowerow o tej samej nazwie");
                 return;
             }
@@ -42,6 +50,9 @@ public class BicycleService {
     }
 
 
+    /* TODO
+      zamień to na toString w obiekcie Bicycle i BicyclePart - kazdy obiekt w JAVA zawiera metode toString(), ktora domyslnie drukuje referencje
+     */
     public void printBicycles() {
         for (Bicycle bicycle : bicycles) {
             System.out.println(bicycle.name);
