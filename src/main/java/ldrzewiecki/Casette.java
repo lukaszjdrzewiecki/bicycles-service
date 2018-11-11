382573ce81d0554f1529ce3e37d27b8dfb85133c
drzew <drzewieckifilip@gmail.com>
2018-11-11 22:48:43 +0100

    casette

--- src/main/java/ldrzewiecki/Casette.java
+++ src/main/java/ldrzewiecki/Casette.java
@@ -6,7 +6,7 @@ public class Casette extends BicyclePart{
     private int maximum;
     private int speed;
 
-    public Casette (){minimum = 0; maximum = 0; speed= 0;}
+
     public Casette(BicyclePartCategory category, String name, int minimum, int maximum, int speed){
         super(category, name);
         this.minimum = minimum;
