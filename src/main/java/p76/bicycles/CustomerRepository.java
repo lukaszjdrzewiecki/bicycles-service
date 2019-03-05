b5702e1fd426b0e1d3b7ca8a73bf1a1aae2d8985
vsxxldr <lukasz.drzewiecki@vps.no>
2019-03-05 20:52:37 +0100

    DB introduced

---
+++ src/main/java/p76/bicycles/CustomerRepository.java
@@ -0,0 +1,12 @@
+package p76.bicycles;
+
+
+import org.springframework.data.repository.CrudRepository;
+import p76.bicycles.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+}
