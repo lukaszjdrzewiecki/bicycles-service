900c1633d0bc48e81444b3e557db53916ec48ef5
vsxxldr <lukasz.drzewiecki@vps.no>
2019-04-25 22:20:06 +0200

    Added reading data from CSV to Database

--- src/main/java/p76/bicycles/ApplicationListener.java
+++ src/main/java/p76/bicycles/ApplicationListener.java
@@ -1,9 +1,15 @@
 package p76.bicycles;
 
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import p76.bicycles.db.entity.*;
 import p76.bicycles.db.entity.brakes.BrakeCaliper;
 import p76.bicycles.db.entity.brakes.BrakeHandleLeft;
@@ -23,6 +29,12 @@ import p76.bicycles.db.entity.wheels.*;
 import p76.bicycles.service.BicycleService;
 import p76.bicycles.service.compatibility.CompatibilityService;
 
+import javax.persistence.EntityManager;
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+@Slf4j
 @Component
 public class ApplicationListener {
 
@@ -32,8 +44,17 @@ public class ApplicationListener {
     @Autowired
     CompatibilityService compatibility;
 
+    @Autowired
+    EntityManager em;
+
+    @Transactional
     @EventListener
     public void onApplicationEvent(ContextRefreshedEvent event) {
+        List<RearDerailleur> rearDerailleurs = loadObjectList(RearDerailleur.class);
+        for(RearDerailleur part : rearDerailleurs) {
+            em.persist(part);
+        }
+
         service.addBicycle("Tormenta Lady", "Accent");
         service.addBicycle("Scale Unique", "Scott");
         service.addBicycle("Trans Siberian 2015", "Kross");
@@ -97,5 +118,20 @@ public class ApplicationListener {
         bicycle.setName(name);
         bicycle.setManufacturer(manufacturer);
         return bicycle;
+    }
+
+    public <T> List<T> loadObjectList(Class<T> type) {
+        String fileName = type.getSimpleName() + ".csv";
+        try {
+            CsvSchema bootstrapSchema = CsvSchema.emptySchema().withHeader().withColumnSeparator(';');
+            CsvMapper mapper = new CsvMapper();
+            File file = new ClassPathResource(fileName).getFile();
+            MappingIterator<T> readValues =
+                    mapper.readerFor(type).with(bootstrapSchema).readValues(file);
+            return readValues.readAll();
+        } catch (Exception e) {
+            log.error("Error occurred while loading object list from file " + fileName, e);
+            return Collections.emptyList();
+        }
     }
 }
