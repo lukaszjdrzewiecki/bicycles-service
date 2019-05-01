df38b18785e848606b7043ce3e88aaea7df2a3ae
vsxxldr <lukasz.drzewiecki@vps.no>
2019-05-01 13:32:25 +0200

    Added repositories for the parts

---
+++ src/main/java/p76/bicycles/db/repository/CrankRestRepository.java
@@ -0,0 +1,12 @@
+package p76.bicycles.db.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import p76.bicycles.db.entity.drivetrain.Crank;
+import p76.bicycles.db.entity.drivetrain.FrontDerailleur;
+
+@CrossOrigin
+@RepositoryRestResource(collectionResourceRel = "crank", path = "crank")
+public interface CrankRestRepository extends PagingAndSortingRepository<Crank, Long> {
+}
--- src/main/java/p76/bicycles/db/repository/FrontDerailleurRestRepository.java
+++ src/main/java/p76/bicycles/db/repository/FrontDerailleurRestRepository.java
@@ -1,15 +1,13 @@
 package p76.bicycles.db.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import p76.bicycles.db.entity.drivetrain.FrontDerailleur;
 import p76.bicycles.db.entity.drivetrain.RearDerailleur;
 
-import java.util.List;
-
 @CrossOrigin
-@RepositoryRestResource(collectionResourceRel = "rearDerailleur", path = "rearDerailleur")
-public interface RearDerailleurRestRepository extends PagingAndSortingRepository<RearDerailleur, Long> {
+@RepositoryRestResource(collectionResourceRel = "frontDerailleur", path = "frontDerailleur")
+public interface FrontDerailleurRestRepository extends PagingAndSortingRepository<FrontDerailleur, Long> {
 
 }
